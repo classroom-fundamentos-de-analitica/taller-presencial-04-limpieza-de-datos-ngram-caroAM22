1cdad2a8ce939eba5538541cbe2e5ccb86bad574
caroAM22 <caalvarezmu@unal.edu.co>
2024-02-26 11:11:10 -0500

    Update taller-04

--- clean_data.py
+++ clean_data.py
@@ -1,41 +1,42 @@
 """Taller evaluable presencial"""
 
 import pandas as pd
+import nltk
 
 
 def load_data(input_file):
     """Lea el archivo usando pandas y devuelva un DataFrame"""
+    data = pd.read_csv(input_file, sep="\t")
+    return data
 
 
 def create_key(df, n):
     """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
 
     df = df.copy()
+    df["fingerprint"] = df["text"]
+    df["fingerprint"] = (
+        df["fingerprint"]
+        .str.strip()
+        .str.lower()
+        .str.translate(str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"))
+        .str.split()
+        .str.join(" ")
+        .apply(lambda x: [x[i : i + n] for i in range(len(x) - n + 1)])
+        .apply(lambda x: [nltk.PorterStemmer().stem(w) for w in x])
+        .apply(lambda x: sorted(set(x)))
+        .str.join(" ")
+    )
 
-    # Copie la columna 'text' a la columna 'key'
-    # Remueva los espacios en blanco al principio y al final de la cadena
-    # Convierta el texto a minúsculas
-    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-    # Remueva puntuación y caracteres de control
-    # Convierta el texto a una lista de tokens
-    # Una el texto sin espacios en blanco
-    # Convierta el texto a una lista de n-gramas
-    # Ordene la lista de n-gramas y remueve duplicados
-    # Convierta la lista de ngramas a una cadena
-    
     return df
 
 
 def generate_cleaned_column(df):
-    """Crea la columna 'cleaned' en el DataFrame"""
-
     df = df.copy()
-
-    # Ordene el dataframe por 'key' y 'text'
-    # Seleccione la primera fila de cada grupo de 'key'
-    # Cree un diccionario con 'key' como clave y 'text' como valor
-    # Cree la columna 'cleaned' usando el diccionario
-
+    df = df.sort_values(by=["fingerprint", "text"]).copy()
+    fingerprints = df.groupby("fingerprint").first().reset_index()
+    fingerprints = fingerprints.set_index("fingerprint")["text"].to_dict()
+    df["cleaned"] = df["fingerprint"].map(fingerprints)
     return df
 
 
